e1672526d40b6836a57f1e0b316944d12d899694
Tony Wu <tonywu3027@me.com>
2021-12-30 03:35:24 +0000

    config: add project config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Project root path
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Data directory
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+# Output directory
+OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
