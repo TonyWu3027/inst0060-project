2b6f0d2dab269d89f4c4ac41b256f650307b7524
Tony Wu <tonywu3027@me.com>
2022-01-21 17:50:38 +0000

    feat(covidcomp.model): implement unregularised logistic regression

--- covidcomp/model.py
+++ covidcomp/model.py
@@ -5,6 +5,7 @@ from numpy import ndarray
 
 from fomlads.evaluate.eval_classification import eval_accuracy
 from fomlads.model.classification import (
+    logistic_regression_fit,
     logistic_regression_predict,
     logistic_regression_prediction_probs,
 )
@@ -61,7 +62,110 @@ class Model(ABC):
         pass
 
 
-class L2RegularisedLogisticRegression(Model):
+class LogisticRegression(Model):
+    """The Logistic Regression
+    implemented with Iteratively Reweighted Least
+    Squares algorithm and Newton-Raphson method
+    """
+
+    def __init__(self, weights0=None, termination_threshold=1e-8, add_bias_term=True):
+        """Instantiate an L-2 Regularised Logistic Regression model
+
+        Args:
+            weights0 (ndarray, optional): the initial weights for iteration.
+                Defaults to None.
+            lambda_ (number, optional): the regularising strength.
+                Defaults to 1e3.
+            termination_threshold (number, optional): [description].
+                Defaults to 1e-8.
+            add_bias_term (bool, optional): [description]. Defaults to True.
+        """
+        self._weights0 = weights0
+        self._add_bias_term = add_bias_term
+        self._termination_threshold = termination_threshold
+
+    def fit(self, inputs: ndarray, targets: ndarray) -> ndarray:
+        """
+        Fits a set of weights to the L-2 regularised logistic regression model
+        using the iteratively reweighted least squares (IRLS) method (Rubin, 1983)
+
+        Adapted from `fomlads.model.classification.logistic_regression_fit`
+        by Dr Luke Dickens, at the Department of Information Studies, UCL
+
+        Args:
+            inputs (ndarray): an N*D matrix, each row is a data-point
+            targets (ndarray): N-dimension vector of class IDs 0 and 1
+
+        Returns:
+            (ndarray): - a set of weights for the model
+        """
+
+        weights = logistic_regression_fit(
+            inputs,
+            targets,
+            weights0=self._weights0,
+            termination_threshold=self._termination_threshold,
+            add_bias_term=self._add_bias_term,
+        )
+        self._weights = weights
+        return weights
+
+    def predict(
+        self,
+        inputs: ndarray,
+        decision_threshold: float = 0.5,
+        add_bias_term: bool = True,
+    ) -> ndarray:
+        """
+        Get deterministic class prediction vector from the logistic regression model.
+
+        Adapted from `fomlads.model.classification.logistic_regression_predict`
+        by Dr Luke Dickens, at the Department of Information Studies, UCL
+
+        Args:
+            inputs (ndarray): an N*D matrix of input data (or design matrix)
+            decision_threshold (float): the prediction probability above which
+                the output prediction is 1. Set to 0.5 for minimum misclassification
+            add_bias_term (bool): whether or not a bias term should be added
+                to the input or the design matrix
+        Returns:
+            (ndarray): an N-dimension vector of predictions
+        """
+        return logistic_regression_predict(
+            inputs, self._weights, decision_threshold, add_bias_term
+        )
+
+    def predict_prob(self, inputs: ndarray) -> ndarray:
+        """Predict the probability of given data inputs to have
+        a target of 1
+
+        Args:
+            inputs (ndarray): an N*D matrix of input data (or design matrix)
+
+        Returns:
+            ndarray: an N-dimension vector of predicted probabilities
+        """
+
+        return logistic_regression_prediction_probs(
+            inputs, self._weights, self._add_bias_term
+        )
+
+    def score(self, test_inputs: ndarray, test_targets: ndarray) -> float:
+        """Return the mean accuracy on the given test inputs and targets.
+
+        Args:
+            test_inputs (ndarray): an N*D inputs/design matrix of data points.
+            test_targets (ndarray): an D-dimension vector of true targets.
+        Returns:
+            (float): the mean accuracy on the given test inputs and targets
+        """
+
+        test_predicts = self.predict(test_inputs)
+        accuracy = eval_accuracy(test_targets, test_predicts)
+        return accuracy
+
+
+class L2RegularisedLogisticRegression(LogisticRegression):
     """The L-2 Regularised Logistic Regression
     implemented with Iteratively Reweighted Least
     Squares algorithm and Newton-Raphson method
@@ -81,10 +185,8 @@ class L2RegularisedLogisticRegression(Model):
                 Defaults to 1e-8.
             add_bias_term (bool, optional): [description]. Defaults to True.
         """
-        self.__weights0 = weights0
-        self.__lambda = lambda_
-        self.__add_bias_term = add_bias_term
-        self.__termination_threshold = termination_threshold
+        super().__init__(weights0, termination_threshold, add_bias_term)
+        self._lambda = lambda_
 
     def fit(self, inputs: ndarray, targets: ndarray) -> ndarray:
         """
@@ -109,27 +211,27 @@ class L2RegularisedLogisticRegression(Model):
         N, D = inputs.shape
         # We should have a bias term (but this may already be included in design
         # matrix). Adding a bias term is equivalent to adding contant first col
-        if self.__add_bias_term:
+        if self._add_bias_term:
             # we do this by making the first column all ones
             inputs = np.hstack((np.ones((N, 1)), inputs))
             # and increasing the apparent dimension
             D += 1
         targets = targets.reshape((N, 1))
         # initialise the weights
-        if self.__weights0 is None:
+        if self._weights0 is None:
             weights = np.random.multivariate_normal(np.zeros(D), 0.001 * np.identity(D))
         else:
-            weights = self.__weights0
+            weights = self._weights0
 
         weights = weights.reshape((D, 1))
         # initially the update magnitude is set as larger than the
         # termination_threshold to ensure the first iteration runs
-        update_magnitude = 2 * self.__termination_threshold
+        update_magnitude = 2 * self._termination_threshold
 
         # Initialise the identity matrix
         identity_matrix = np.identity(D)
 
-        while update_magnitude > self.__termination_threshold:
+        while update_magnitude > self._termination_threshold:
             print(update_magnitude)
             # calculate the current prediction vector for weights
             # we don't want to extend the inputs a second time so add_bias_term
@@ -143,53 +245,16 @@ class L2RegularisedLogisticRegression(Model):
             predicts = predicts.reshape((N, 1))
             # Calculate the Hessian inverse
             H_inv = np.linalg.inv(
-                inputs.T @ R @ inputs + self.__lambda * identity_matrix
+                inputs.T @ R @ inputs + self._lambda * identity_matrix
             )
             # Calculate the gradient
-            gradient = inputs.T @ (predicts - targets) + self.__lambda * weights
+            gradient = inputs.T @ (predicts - targets) + self._lambda * weights
             # update the weights
             new_weights = weights - H_inv @ gradient
             # calculate the update_magnitude
             update_magnitude = np.sqrt(np.sum((new_weights - weights) ** 2))
             # update the weights
             weights = new_weights
-        self.__weights = weights
+        self._weights = weights
 
-    def predict(
-        self,
-        inputs: ndarray,
-        decision_threshold: float = 0.5,
-        add_bias_term: bool = True,
-    ) -> ndarray:
-        """
-        Get deterministic class prediction vector from the logistic regression model.
-
-        Adapted from `fomlads.model.classification.logistic_regression_predict`
-        by Dr Luke Dickens, at the Department of Information Studies, UCL
-
-        Args:
-            inputs (ndarray): an N*D matrix of input data (or design matrix)
-            decision_threshold (float): the prediction probability above which
-                the output prediction is 1. Set to 0.5 for minimum misclassification
-            add_bias_term (bool): whether or not a bias term should be added
-                to the input or the design matrix
-        Returns:
-            (ndarray): an N-dimension vector of predictions
-        """
-        return logistic_regression_predict(
-            inputs, self.__weights, decision_threshold, add_bias_term
-        )
-
-    def score(self, test_inputs: ndarray, test_targets: ndarray) -> float:
-        """Return the mean accuracy on the given test inputs and targets.
-
-        Args:
-            test_inputs (ndarray): an N*D inputs/design matrix of data points.
-            test_targets (ndarray): an D-dimension vector of true targets.
-        Returns:
-            (float): the mean accuracy on the given test inputs and targets
-        """
-
-        test_predicts = self.predict(test_inputs)
-        accuracy = eval_accuracy(test_targets, test_predicts)
-        return accuracy
+        return weights
